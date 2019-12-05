d3cf4335d2f822f90d92e35ebb423c04d85d6123
rustky <50724477+rustky@users.noreply.github.com>
2019-12-04 19:23:54 -0700

    Update Transmitter_Code2.0.ino

--- Transmitter_Code2.0.ino
+++ Transmitter_Code2.0.ino
@@ -1,31 +1,32 @@
-#include <SPI.h>  
+#include <SPI.h>  //Include libraries needed to call certain methods
 #include "RF24.h"
 #include <nRF24L01.h>
-const byte address[6] = "00001";
-RF24 myRadio (9, 10);
+const byte address[6] = "00001";  //Sets up transmission address
+RF24 myRadio (9, 10);             //Sets pins 9 and 10 on the Arduino as the transciever radio pins
 
 void setup()
 {
-  Serial.begin(9600);
-  delay(1000);
-  myRadio.begin();
-  myRadio.setPALevel(RF24_PA_MIN); 
-  myRadio.openWritingPipe(address);
-  myRadio.stopListening();
-  delay(1000);
+  Serial.begin(9600);             //Begins serial monitor for debugging
+  delay(1000);                    //Delay to let things boot up
+  myRadio.begin();                //Begins radio transmission
+  myRadio.setPALevel(RF24_PA_MIN);  //Sets radio power level to low
+  myRadio.openWritingPipe(address);  //Opens radio transmitting line
+  myRadio.stopListening();          //Ensures transciever is not trying to receive any data
+  delay(1000);                    //Short delay
 }
-void loop(){
-  delay(10);
-  int yAxis = analogRead(A2);
-  myRadio.write(&yAxis, sizeof(yAxis));
-  Serial.println(yAxis);
+//Everything below this line was written by student
+void loop(){                      //Loops infinitely
+  delay(10);                      //Delay to try to line up transmission timing with the receiver
+  int yAxis = analogRead(A2);     //Reads the value of the y angle of the joystick from analog pin 2 and stores as an integer
+  myRadio.write(&yAxis, sizeof(yAxis)); //Sends the value of y angle and the size of the data
+  Serial.println(yAxis);          //Prints y angle for debugging
   
-  delay(10);
+  delay(10);                      //Delay for timing with receiver
   
-  int xAxis = analogRead(A1);
-  myRadio.write(&xAxis, sizeof(xAxis));
-  Serial.println(xAxis);
+  int xAxis = analogRead(A1);     //Reads the value of the x angle of the joystick from analog pin 1 and stores as an integer
+  myRadio.write(&xAxis, sizeof(xAxis)); //Sends the value of x angle and the size of the data
+  Serial.println(xAxis);          //Prints x angle for debugging
   
 
-  delay(10);
+  delay(10);                      //Short delay before looping
 }
